32e808c2aba3835113f2ed18841ed3df34e8669a
Ozoku <dragon.djozoku@gmail.com>
2021-09-02 16:20:07 +0300

    remove duplicate refined storage large storage

---
+++ kubejs/server_scripts/removes.js
@@ -0,0 +1,20 @@
+onEvent("recipes", (event) => {
+  const sizes = [
+    '256k',
+    '1024k',
+    '4096k',
+    '16384k',
+    '16384k_fluid',
+    '65536k_fluid',
+    '262144k_fluid',
+    '1048576k_fluid'
+  ];
+  
+  const items = [
+    ...sizes.map(size => `extrastorage:storagepart_${size}`),
+    ...sizes.map(size => `extrastorage:disk_${size}`),
+    ...sizes.map(size => `extrastorage:block_${size}`)
+  ];
+  
+  items.forEach((item) => event.remove({ output: item }));
+});
